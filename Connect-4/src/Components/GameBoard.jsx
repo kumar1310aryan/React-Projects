beb6a00c028b2ca93bb4ea9f55dbc08322fde6cf
kumar1310aryan <kumar1310aryan@gmail.com>
2024-08-21 08:39:43 +0530

    passing arguments to onclick

--- Connect-4/src/Components/GameBoard.jsx
+++ Connect-4/src/Components/GameBoard.jsx
@@ -1,8 +1,28 @@
+import GameCircle from "./GameCircle";
+
 const GameBoard = () => {
   return (
     <>
-      <div className="">
-        <h1>This is the Game Board.</h1>
+      <div>
+        <GameCircle id={1}>red</GameCircle>
+        <GameCircle id={2}>blue</GameCircle>
+        <GameCircle id={3}>red</GameCircle>
+        <GameCircle id={4}>blue</GameCircle>
+
+        <GameCircle id={5}>red</GameCircle>
+        <GameCircle id={6}>blue</GameCircle>
+        <GameCircle id={7}>red</GameCircle>
+        <GameCircle id={8}>blue</GameCircle>
+
+        <GameCircle id={9}>red</GameCircle>
+        <GameCircle id={10}>blue</GameCircle>
+        <GameCircle id={11}>red</GameCircle>
+        <GameCircle id={12}>blue</GameCircle>
+
+        <GameCircle id={13}>red</GameCircle>
+        <GameCircle id={14}>blue</GameCircle>
+        <GameCircle id={15}>red</GameCircle>
+        <GameCircle id={16}>blue</GameCircle>
       </div>
     </>
   );
---
+++ Connect-4/src/Components/GameCircle.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+const onClick = (id) => {
+  alert("clicked " + id);
+};
+
+const GameCircle = ({ id, children }) => {
+  debugger;
+  console.log(id);
+  return (
+    <>
+      <div className="circle" onClick={() => onClick(id)}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+GameCircle.propTypes = {
+  id: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+};
+
+export default GameCircle;
