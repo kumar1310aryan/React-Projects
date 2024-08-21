7c7204914ca29bed6781b12ef406a1ca49c1cdff
kumar1310aryan <kumar1310aryan@gmail.com>
2024-08-21 11:04:31 +0530

    circles

--- Connect-4/src/Components/GameBoard.jsx
+++ Connect-4/src/Components/GameBoard.jsx
@@ -3,58 +3,32 @@ import GameCircle from "./GameCircle";
 const GameBoard = () => {
   return (
     <>
-      <div>
-        <GameCircle id={1}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={2}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
-        <GameCircle id={3}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={4}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          gridTemplateRows: "1fr 1fr 1fr 1fr",
+        }}
+      >
+        <GameCircle id={1} backgroundColor="red"></GameCircle>
+        <GameCircle id={2} backgroundColor="blue"></GameCircle>
+        <GameCircle id={3} backgroundColor="red"></GameCircle>
+        <GameCircle id={4} backgroundColor="blue"></GameCircle>
 
-        <GameCircle id={5}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={6}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
-        <GameCircle id={7}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={8}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
+        <GameCircle id={5} backgroundColor="red"></GameCircle>
+        <GameCircle id={6} backgroundColor="blue"></GameCircle>
+        <GameCircle id={7} backgroundColor="red"></GameCircle>
+        <GameCircle id={8} backgroundColor="blue"></GameCircle>
 
-        <GameCircle id={9}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={10}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
-        <GameCircle id={11}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={12}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
+        <GameCircle id={9} backgroundColor="red"></GameCircle>
+        <GameCircle id={10} backgroundColor="blue"></GameCircle>
+        <GameCircle id={11} backgroundColor="red"></GameCircle>
+        <GameCircle id={12} backgroundColor="blue"></GameCircle>
 
-        <GameCircle id={13}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={14}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
-        <GameCircle id={15}>
-          <span style={{ color: "red" }}>red</span>
-        </GameCircle>
-        <GameCircle id={16}>
-          <span style={{ color: "blue" }}>blue</span>
-        </GameCircle>
+        <GameCircle id={13} backgroundColor="red"></GameCircle>
+        <GameCircle id={14} backgroundColor="blue"></GameCircle>
+        <GameCircle id={15} backgroundColor="red"></GameCircle>
+        <GameCircle id={16} backgroundColor="blue"></GameCircle>
       </div>
     </>
   );
--- Connect-4/src/Components/GameCircle.jsx
+++ Connect-4/src/Components/GameCircle.jsx
@@ -4,11 +4,21 @@ const onClick = (id) => {
   alert("clicked " + id);
 };
 
-const GameCircle = ({ id, children }) => {
+const GameCircle = ({ id, children, backgroundColor }) => {
   console.log(id);
   return (
     <>
-      <div className="circle" onClick={() => onClick(id)}>
+      <div
+        className="circle"
+        onClick={() => onClick(id)}
+        style={{
+          backgroundColor: backgroundColor,
+          height: 100,
+          width: 100,
+          margin: 10,
+          borderRadius: 50,
+        }}
+      >
         {children}
       </div>
     </>
@@ -18,6 +28,7 @@ const GameCircle = ({ id, children }) => {
 GameCircle.propTypes = {
   id: PropTypes.string.isRequired,
   children: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired,
 };
 
 export default GameCircle;
