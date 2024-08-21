84fbb3acf04239c6b812aacc23299c1d625bff30
kumar1310aryan <kumar1310aryan@gmail.com>
2024-08-21 10:44:18 +0530

    inline styles

--- Connect-4/src/Components/GameBoard.jsx
+++ Connect-4/src/Components/GameBoard.jsx
@@ -4,25 +4,57 @@ const GameBoard = () => {
   return (
     <>
       <div>
-        <GameCircle id={1}>red</GameCircle>
-        <GameCircle id={2}>blue</GameCircle>
-        <GameCircle id={3}>red</GameCircle>
-        <GameCircle id={4}>blue</GameCircle>
+        <GameCircle id={1}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={2}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
+        <GameCircle id={3}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={4}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
 
-        <GameCircle id={5}>red</GameCircle>
-        <GameCircle id={6}>blue</GameCircle>
-        <GameCircle id={7}>red</GameCircle>
-        <GameCircle id={8}>blue</GameCircle>
+        <GameCircle id={5}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={6}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
+        <GameCircle id={7}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={8}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
 
-        <GameCircle id={9}>red</GameCircle>
-        <GameCircle id={10}>blue</GameCircle>
-        <GameCircle id={11}>red</GameCircle>
-        <GameCircle id={12}>blue</GameCircle>
+        <GameCircle id={9}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={10}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
+        <GameCircle id={11}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={12}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
 
-        <GameCircle id={13}>red</GameCircle>
-        <GameCircle id={14}>blue</GameCircle>
-        <GameCircle id={15}>red</GameCircle>
-        <GameCircle id={16}>blue</GameCircle>
+        <GameCircle id={13}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={14}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
+        <GameCircle id={15}>
+          <span style={{ color: "red" }}>red</span>
+        </GameCircle>
+        <GameCircle id={16}>
+          <span style={{ color: "blue" }}>blue</span>
+        </GameCircle>
       </div>
     </>
   );
