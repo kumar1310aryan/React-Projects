9ef64af0762f947717c6cc7420d2d8dbd7f37fe1
kumar1310aryan <kumar1310aryan@gmail.com>
2024-08-21 07:44:44 +0530

    Connect-4 game board

---
+++ Connect-4/src/App.jsx
@@ -0,0 +1,11 @@
+import GameBoard from "./Components/GameBoard";
+
+function App() {
+  return (
+    <>
+      <GameBoard />
+    </>
+  );
+}
+
+export default App;
---
+++ Connect-4/src/Components/GameBoard.jsx
@@ -0,0 +1,11 @@
+const GameBoard = () => {
+  return (
+    <>
+      <div className="">
+        <h1>This is the Game Board.</h1>
+      </div>
+    </>
+  );
+};
+
+export default GameBoard;
